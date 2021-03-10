b47286144061077b134249e606390104cc544e39
Aymerick Valette <valaymerick@gmail.com>
2021-03-10 10:01:01 +0100

    generate subdivisions

---
+++ build.rs
@@ -0,0 +1,105 @@
+use std::{env, error::Error, path::Path};
+
+use serde::{Deserialize, Serialize};
+use tinytemplate::TinyTemplate;
+
+/// A country as represented by the ISO 3166-1 standard
+#[derive(Serialize, Deserialize)]
+struct Country {
+    alpha_2: String,
+    alpha_3: String,
+    name: String,
+    numeric: u16,
+}
+
+/// A country subdivision as represented by the ISO 3166-2 standard
+#[derive(Deserialize)]
+struct Subdivision {
+    code: String,
+    name: String,
+    #[serde(rename = "type")]
+    ty: String,
+    parent: Option<String>,
+}
+
+#[derive(Deserialize)]
+struct SubdivisionDataset {
+    #[serde(rename(deserialize = "3166-2"))]
+    subdivisions: Vec<Subdivision>,
+}
+
+#[derive(Deserialize, Serialize)]
+struct CountryDataset {
+    #[serde(rename(deserialize = "3166-1"))]
+    countries: Vec<Country>,
+}
+
+// -----------------------------------------------------------------------------
+
+#[derive(Serialize)]
+struct SubdivisionTemplate {
+    code: String,
+    code_identifier: String,
+    name: String,
+    #[serde(rename = "type")]
+    ty: String,
+    parent: Option<String>,
+}
+
+#[derive(Serialize)]
+struct TemplateContext {
+    subdivisions: Vec<SubdivisionTemplate>,
+}
+
+impl From<Subdivision> for SubdivisionTemplate {
+    fn from(value: Subdivision) -> Self {
+        Self {
+            code_identifier: str::replace(value.code.as_str(), "-", "_"),
+            code: value.code,
+            name: value.name,
+            ty: value.ty,
+            parent: value.parent,
+        }
+    }
+}
+
+// -----------------------------------------------------------------------------
+
+fn generate_subdivision_file(
+    template: &str,
+    values: impl Iterator<Item = Subdivision>,
+) -> Result<String, Box<dyn Error>> {
+    let mut tt = TinyTemplate::new();
+    tt.add_template("subdivision", template)?;
+
+    let ctx = TemplateContext {
+        subdivisions: values.map(Into::into).collect(),
+    };
+
+    Ok(tt.render("subdivision", &ctx)?)
+}
+
+// -----------------------------------------------------------------------------
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let iso_3166_2_dataset = std::fs::read_to_string("build/data/data_iso_3166-2.json")?;
+    let subdivision_dataset: SubdivisionDataset =
+        serde_json::from_str(iso_3166_2_dataset.as_str())?;
+
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join("subdivision.rs");
+
+    let subdivision_template = std::fs::read_to_string("build/subdivision.template")?;
+
+    std::fs::write(
+        dest_path,
+        generate_subdivision_file(
+            subdivision_template.as_str(),
+            subdivision_dataset.subdivisions.into_iter(),
+        )
+        .expect("failed to generate subdivisions"),
+    )
+    .unwrap();
+
+    Ok(())
+}
---
+++ src/subdivision.rs
@@ -0,0 +1,86 @@
+#![allow(non_camel_case_types, dead_code)]
+use serde::{
+    de::{self, Visitor},
+    Serialize, Serializer,
+};
+use std::{
+    fmt::{self, Display},
+    str::FromStr,
+};
+use thiserror::Error;
+
+/// An administrative subdivision as specified by the ISO 3166-2 standard
+pub struct Subdivision {
+    code: Code,
+    name: &'static str,
+    ty: &'static str,
+}
+
+impl Subdivision {
+    /// Returns the subdivision code
+    pub fn code(&self) -> Code {
+        self.code
+    }
+
+    /// Returns the subdivision name in the language corresponding to the given country
+    pub fn name(&self) -> &'static str {
+        self.name
+    }
+
+    /// Returns the subdivision type specific to the given country
+    pub fn ty(&self) -> &'static str {
+        self.ty
+    }
+}
+
+// -----------------------------------------------------------------------------
+
+include!(concat!(env!("OUT_DIR"), "/subdivision.rs"));
+
+// -----------------------------------------------------------------------------
+
+impl Serialize for Subdivision {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.collect_str(&self.code)
+    }
+}
+
+struct SubdivisionVisitor;
+
+impl<'de> Visitor<'de> for SubdivisionVisitor {
+    type Value = Code;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "an ISO 3166-1 compliant alpha-2 country code")
+    }
+
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+    where
+        E: de::Error,
+    {
+        match Code::from_str(v) {
+            Ok(x) => Ok(x),
+            Err(_) => Err(de::Error::invalid_value(de::Unexpected::Str(v), &self)),
+        }
+    }
+
+    fn visit_borrowed_str<E>(self, v: &'de str) -> Result<Self::Value, E>
+    where
+        E: de::Error,
+    {
+        match Code::from_str(v) {
+            Ok(x) => Ok(x),
+            Err(_) => Err(de::Error::invalid_value(de::Unexpected::Str(v), &self)),
+        }
+    }
+}
+
+/// Errors that might arise when converting raw data into a subdivision code
+#[derive(Debug, Error, PartialEq)]
+pub enum SubdivisionCodeParseError {
+    #[error("invalid iso subdivison code string")]
+    InvalidCode,
+}
