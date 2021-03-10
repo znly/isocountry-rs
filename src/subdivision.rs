757046570712a837d305a994846a4df0eaedaa7f
Aymerick Valette <valaymerick@gmail.com>
2021-03-10 20:13:07 +0100

    from<code> for subdivision and human tests

--- src/subdivision.rs
+++ src/subdivision.rs
@@ -1,9 +1,8 @@
 #![allow(non_camel_case_types, dead_code)]
-
 #[cfg(feature = "serde1")]
 use serde::{
     de::{self, Visitor},
-    Deserialize, Serialize, Serializer,
+    Deserialize, Deserializer, Serialize, Serializer,
 };
 use std::{
     fmt::{self, Display},
@@ -11,6 +10,7 @@ use std::{
 };
 use thiserror::Error;
 
+#[derive(Debug, PartialEq)]
 /// An administrative subdivision as specified by the ISO 3166-2 standard
 pub struct Subdivision {
     code: Code,
@@ -35,12 +35,6 @@ impl Subdivision {
     }
 }
 
-// -----------------------------------------------------------------------------
-
-include!(concat!(env!("OUT_DIR"), "/subdivision.rs"));
-
-// -----------------------------------------------------------------------------
-
 #[cfg(feature = "serde1")]
 impl Serialize for Subdivision {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
@@ -52,11 +46,21 @@ impl Serialize for Subdivision {
 }
 
 #[cfg(feature = "serde1")]
+impl<'de> Deserialize<'de> for Subdivision {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_str(SubdivisionVisitor)
+    }
+}
+
+#[cfg(feature = "serde1")]
 struct SubdivisionVisitor;
 
 #[cfg(feature = "serde")]
 impl<'de> Visitor<'de> for SubdivisionVisitor {
-    type Value = Code;
+    type Value = Subdivision;
 
     fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
         write!(formatter, "an ISO 3166-1 compliant alpha-2 country code")
@@ -67,7 +71,7 @@ impl<'de> Visitor<'de> for SubdivisionVisitor {
         E: de::Error,
     {
         match Code::from_str(v) {
-            Ok(x) => Ok(x),
+            Ok(x) => Ok(x.into()),
             Err(_) => Err(de::Error::invalid_value(de::Unexpected::Str(v), &self)),
         }
     }
@@ -77,7 +81,7 @@ impl<'de> Visitor<'de> for SubdivisionVisitor {
         E: de::Error,
     {
         match Code::from_str(v) {
-            Ok(x) => Ok(x),
+            Ok(x) => Ok(x.into()),
             Err(_) => Err(de::Error::invalid_value(de::Unexpected::Str(v), &self)),
         }
     }
@@ -90,9 +94,16 @@ pub enum SubdivisionCodeParseError {
     InvalidCode,
 }
 
+// -----------------------------------------------------------------------------
+
+include!(concat!(env!("OUT_DIR"), "/subdivision.rs"));
+
+// -----------------------------------------------------------------------------
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use serde_json::{self, json};
 
     #[test]
     fn convert_from_and_to_string() {
@@ -115,6 +126,54 @@ mod tests {
                 let str = actual.to_string();
                 assert_eq!(str, raw.to_string());
             }
+        }
+    }
+
+    #[test]
+    #[cfg(feature = "serde1")]
+    fn serialize_subdivision() {
+        let arizona: Subdivision = Subdivision {
+            name: "Arizona",
+            ty: "State",
+            code: Code::US_AZ,
+        };
+
+        let california: Subdivision = Subdivision {
+            name: "California",
+            ty: "State",
+            code: Code::US_CA,
+        };
+
+        let colorado: Subdivision = Subdivision {
+            name: "Colorado",
+            ty: "State",
+            code: Code::US_CO,
+        };
+
+        let connecticut: Subdivision = Subdivision {
+            name: "Connecticut",
+            ty: "State",
+            code: Code::US_CT,
+        };
+
+        let tests = [
+            (arizona, "US-AZ"),
+            (california, "US-CA"),
+            (colorado, "US-CO"),
+            (connecticut, "US-CT"),
+        ];
+
+        for (subdivision, expected) in &tests {
+            let actual =
+                serde_json::to_value(subdivision).expect("should serialize the subdivision");
+
+            assert_eq!(json!(expected), actual);
+
+            // re-convert
+            let converted: Subdivision =
+                serde_json::from_value(actual).expect("should deserialize the subdivision");
+
+            assert_eq!(subdivision, &converted);
         }
     }
 }
